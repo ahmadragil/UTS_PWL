a88901aa01cb8c5cc183af4410727ba0f8306776
ahmadragil <ragilunisnu@gmail.com>
2020-05-01 22:42:10 +0700

    Saya memasukkan folder CI ke folder Repository

---
+++ application/controllers/Auth.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('auth/login');	
+	}
+
+	public function process()
+	{
+		$post = $this->input->post(null, TRUE);
+		if(isset($post['login'])){
+			$this->load->model('user_m');
+			$query  =$this->user_m->login($post);
+			if($query->num_rows() > 0){
+				$row = $query->row();
+				$params = array(
+					'userid' => $row->id,
+					'level' => $row->level
+				);
+				$this->session->set_userdata($params);
+				echo "<script>
+					alert ('Selamat, Login Anda Berhasil');
+					window.location='".base_url('dashboard')."';
+				</script>";
+			}else {
+				echo "<script>
+					alert ('Login Gagal, username / password salah');
+					window.location='".base_url('auth/login')."';
+					</script>";
+				}	
+			}
+		}
+	}
+
